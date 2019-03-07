dd3e724ddbb744160cad7015e3d8095594aa53d8
mananwalia959 <mananwalia959@gmail.com>
2019-03-07 16:30:52 +0530

    setup env variables

--- index.js
+++ index.js
@@ -1,3 +1,17 @@
+require('dotenv').config({ path:'./variables.env'})//Setting up Enviorment Varables First and Foremost
 
+const express= require('express');
+
+const app = express();
+const port = process.env.PORT || 8080;
+
+
+app.get('/',(req,res)=>{
+    res.json('Hello World');
+})
+
+app.listen(port,()=>{
+    console.log(`Listening on port ${port}`)
+})
 
 
