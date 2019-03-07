f61b1350e48d75a692e62f07714b4dd520be3538
mananwalia959 <mananwalia959@gmail.com>
2019-03-07 17:00:51 +0530

    Express , mongo and Angular boilerplate setup

--- index.js
+++ index.js
@@ -1,8 +1,25 @@
 require('dotenv').config({ path:'./variables.env'})//Setting up Enviorment Varables First and Foremost
 
+/**
+ * Routing App Setup
+ */
 const express= require('express');
-
 const app = express();
+
+
+const mongoose = require('mongoose');
+mongoose.connect(process.env.MONGODBURL,
+    { useNewUrlParser: true })
+.then(()=>{
+    console.log(`Database Connected Successfully`);
+})
+.catch((err)=>{
+    console.log(err)
+})
+
+
+
+
 const port = process.env.PORT || 8080;
 
 
